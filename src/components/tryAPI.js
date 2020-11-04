3b600aa52f065651c045be4f0bbfb54f556695f2
unknown <18uec169@lnmiit.ac.in>
2020-11-04 20:36:56 +0530

    setting up react

---
+++ src/components/tryAPI.js
@@ -0,0 +1,29 @@
+// import React,{Component} from 'react';
+// // import './App.css';
+// import axios from 'axios';
+
+// function App() {
+//   const getPosts = () => {
+//     axios
+//       .get('https://jsonplaceholder.typicode.com/posts')
+//       .then((posts) => setPosts(posts.data))
+//       .catch((error) => console.log(error));
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Blog Post</h1>
+//       <button onClick ={() => getPosts()}>
+//         Get  post
+//       </button>
+//       { posts.map((post) => (
+//           <div key={post.id}>
+//             <h3>{post.title} </h3>
+//             <p> {post.body} </p>
+//           </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
